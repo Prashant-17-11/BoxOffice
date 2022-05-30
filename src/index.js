65cc3e8b4d531f478ee5e6a3033c03ae8c833e2e
Prashant Singh <prashantsingh.999000@gmail.com>
2022-05-30 08:18:53 +0530

    added routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Starred from "./Routes/Starred";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+serviceWorkerRegistration.register();
+
+ReactDOM.render(
+  <BrowserRouter>
+  <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="starred" element={<Starred />}>
+        </Route>
+        <Route path="*" element={<>This is 404 page.</>}></Route> {/* '*' matches only no other routes match */}
+      </Routes>
+  </BrowserRouter>, // wrapping app component
+  document.getElementById("root")
 );
