0e2aa98dcc3672853c207c9dc3625ce8417e2715
Prashant Singh <prashantsingh.999000@gmail.com>
2022-05-30 09:50:31 +0530

    added fetch results from tv maze api and rendered the results

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import MainPageLayout from "../Components/MainPageLayout";
+import { apiGet } from "../Misc/Config";
 
 const Home = () => {
-  return <MainPageLayout>This is Home Page!!</MainPageLayout>;
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState(null);
+
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode == 13) {
+      onSearch();
+    }
+  };
+  // 13 is keyCode for ENTER key
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
 };
 
 export default Home;
