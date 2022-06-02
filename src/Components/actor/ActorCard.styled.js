8e9ed59d84f2b821a56ac996ebde7706b5927f50
Prashant Singh <prashantsingh.999000@gmail.com>
2022-06-02 23:13:52 +0530

    installed styled-components and added card styling

---
+++ src/Components/actor/ActorCard.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { SerachCard } from "../Styled";
+
+export const StyledActorCard = styled.div`
+    .deathday {
+        margin: 0;
+        margin-top: 15px;
+        font-weight: bold;
+    }
+`;
