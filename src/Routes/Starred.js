c474b83206431dfd796a906b4d1cac9d2d962ee6
Prashant Singh <prashantsingh.999000@gmail.com>
2022-06-07 12:07:22 +0530

    completed starred component

--- src/Routes/Starred.js
+++ src/Routes/Starred.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainPageLayout from "../Components/MainPageLayout";
+import { useShows } from "../Misc/CustomHooks";
+import { apiGet } from "../Misc/Config";
+import ShowGrid from "../Components/show/ShowGrid";
 
 const Starred = () => {
-  return <MainPageLayout>This is starred page!</MainPageLayout>;
+  const [starred] = useShows();
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map((showId) => apiGet(`/shows/${showId}`));
+
+      Promise.all(promises)
+        .then((apiData) => apiData.map((show) => ({ show })))
+        .then((results) => {
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {isLoading && <div>Shows are still loading</div>}
+      {error && <div>Error occured: {error}</div>}
+      {!isLoading && !shows && <div>No shows were added</div>}
+      {!isLoading && !error && shows ** <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Starred;
