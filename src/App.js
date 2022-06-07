cfae66cc97f5c35ae0199da3a82261419be140fd
Prashant Singh <prashantsingh.999000@gmail.com>
2022-06-07 11:27:39 +0530

    added child components for shows

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import Home from "./Routes/Home";
 import Starred from "./Routes/Starred";
+import Show from "./Routes/Show";
+
+const theme = {
+  mainColors: {
+    blue: "#2400ff",
+    gray: "#c6c6c6",
+    dark: "#353535",
+  },
+};
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="starred" element={<Starred />}>
-        </Route>
-        <Route path="*" element={<>This is 404 page.</>}></Route> {/* '*' matches only no other routes match */}
-     </Routes>
-  
-    </div>
+        <Route path="starred" element={<Starred />}></Route>
+        <Route path="/show/:id" element={<Show />}></Route>
+        <Route path="*" element={<>This is 404 page.</>}></Route>{" "}
+        {/* '*' matches only no other routes match */}
+      </Routes>
+    </ThemeProvider>
   );
 }
 
